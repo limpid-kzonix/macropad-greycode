5ee4c67f98d483deafca7a88621cd833bb3a188a
limpid-kzonix <balyszyn@gmail.com>
2025-02-23 02:43:06 +0200

    feat(keybs): inital draft

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ byte rows[ROWS_NUM] = {D19, D5, A9};
 byte cols[COLS_NUM] = {A3, A2, A1};
 
 char keys[ROWS_NUM][COLS_NUM] = {
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'}};
+    {'3', '6', '9'},
+    {'2', '5', '8'},
+    {'1', '4', '7'}};
 
 Keypad keypad = Keypad(makeKeymap(keys), rows, cols, ROWS_NUM, COLS_NUM);
 
@@ -27,11 +27,18 @@ void setup()
 
 void loop()
 {
-  int val = keypad.getKey();
+  int keys = keypad.getKeys();
+  int key = keypad.getState();
 
-  if (val != NO_KEY)
+  if (keys)
   {
-    Serial.print("Key pressed: ");
-    Serial.println(val);
+    for (int i = 0; i < LIST_MAX; i++)
+    {
+      if (keypad.key[i].kstate == PRESSED)
+      {
+        Serial.print("Key Pressed: ");
+        Serial.println(keypad.key[i].kchar);
+      }
+    }
   }
 }
